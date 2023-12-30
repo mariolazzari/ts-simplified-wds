2bc6d4dad463da8cedbe807b9eb73b0de1361a49
Mario Lazzari <mario.lazzari@gmail.com>
2023-12-30 15:17:42 +0100

    Done lesson 5

---
+++ ts-npm/script.js
@@ -0,0 +1,5 @@
+var num1 = 1;
+var num2 = 2;
+function add(a, b) {
+    return a + b;
+}
---
+++ ts-npm/script.ts
@@ -0,0 +1,6 @@
+const num1 = 1;
+const num2 = 2;
+
+function add(a: number, b: number) {
+  return a + b;
+}
